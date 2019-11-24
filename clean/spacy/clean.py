e0af321abeea10aa76580f90684dfa72e7d35690
Yili Yang <yili.yang@columbia.edu>
2019-11-24 17:59:54 -0500

    spacy clean
    including:
    lemma
    tokenize
    pos
    stopword
    punct
    emoji

--- clean/spacy/clean.py
+++ clean/spacy/clean.py
@@ -1,8 +1,12 @@
 import spacy
+from spacymoji import Emoji
 
 class Clean():
-    def __init__():
+
+    def __init__(self):
         self.nlp = spacy.load('en_core_web_sm')
+        emoji = Emoji(self.nlp)
+        self.nlp.add_pipe(emoji, first=True)
 
     def lemma(self,text):
         return ' '.join([i.lemma_ for i in self.nlp(text)])
@@ -13,8 +17,11 @@ class Clean():
     def posTagger(self,text):
         return [i.pos_ for i in self.nlp(text)]
     
-    def removeStopWords(self,text):
+    def removeStopWord(self,text):
         return [i.text for i in self.nlp(text) if not i.stop_]
     
-    def 
+    def removePunct(self,text):
+        return [i.text for i in self.nlp(text) if not i.is_punct]
     
+    def removeEmoji(self,text):
+        return [i.text for i in self.nlp(text) if not i.is_emoji]
